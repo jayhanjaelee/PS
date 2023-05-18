f72d9d10d8349ebbe54aad8fb5629fa37404b1ee
Hanjae Lee <jayhanjaelee@gmail.com>
2023-05-18 18:51:27 +0900

    modify: indent.

--- implementation/FizzBuzz.java
+++ implementation/FizzBuzz.java
@@ -3,29 +3,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class FizzBuzz {
-  public void solution() throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int x = Integer.parseInt(st.nextToken());
-    int y = Integer.parseInt(st.nextToken());
-    int n = Integer.parseInt(st.nextToken());
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-    for (int i = 1; i <= n; i++) {
-      if (i % x == 0) {
-        if (i % y == 0) {
-          System.out.println("FizzBuzz");
-        } else {
-          System.out.println("Fizz");
+        for (int i = 1; i <= n; i++) {
+            if (i % x == 0) {
+                if (i % y == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
+            } else if (i % y == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
-      } else if (i % y == 0) {
-        System.out.println("Buzz");
-      } else {
-        System.out.println(i);
-      }
     }
-  }
 
-  public static void main(String[] args) throws Exception {
-    new FizzBuzz().solution();
-  }
+    public static void main(String[] args) throws Exception {
+        new FizzBuzz().solution();
+    }
 }
