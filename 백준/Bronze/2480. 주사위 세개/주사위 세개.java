0b501341b910d3cf3e19d2c1afeac89ec2d986d3
Hanjae Lee <122243107+jayhanjaelee@users.noreply.github.com>
2023-07-05 18:39:38 +0900

    [Bronze IV] Title: 주사위 세개, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int ans = 0;
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a == b && a == c && b == c) {
+            ans = 10000 + (a * 1000);
+        } else if (a == b || a == c) {
+            ans = 1000 + (a * 100);
+        } else if (b == c) {
+            ans = 1000 + (b * 100);
+        } else {
+            ans = Math.max(a, Math.max(b, c)) * 100;
+        }
+
+        System.out.println(ans);
+    }
+}
