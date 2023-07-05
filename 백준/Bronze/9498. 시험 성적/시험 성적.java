80d9fc7da4005f8603110f83878d81e11eacc768
Hanjae Lee <122243107+jayhanjaelee@users.noreply.github.com>
2023-07-05 17:26:14 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        int grade = Integer.parseInt(br.readLine());
+
+        if (grade >= 90) {
+            System.out.println("A");
+        } else if (grade >= 80) {
+            System.out.println("B");
+        } else if (grade >= 70) {
+            System.out.println("C");
+        } else if (grade >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+        br.close();
+    }
+}
